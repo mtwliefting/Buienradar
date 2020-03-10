14f81fa2d525c2ac75c378261a9f7c20aa0dcaa1
Mander Liefting <52751000+mtwliefting@users.noreply.github.com>
2020-03-10 18:19:42 +0100

    Update buienradar_pandas.py

--- buienradar_pandas.py
+++ buienradar_pandas.py
@@ -7,12 +7,8 @@ pd.set_option('display.max_rows', 300)
 pd.set_option('display.max_columns', 8)
 pd.set_option('display.width', 200)
 
-
 data = urllib2.urlopen('https://data.buienradar.nl/2.0/feed/json')
 data = json.loads(data.read())
-#temp = [40]
-#a = 1
-
 
 df = pd.DataFrame(data["actual"]["stationmeasurements"],columns=['stationname','temperature','humidity','weatherdescription'])
 df['temperature'] = df['temperature'].fillna(0)
@@ -22,8 +18,3 @@ print(df)
 
 df2 = pd.DataFrame(data["forecast"]["fivedayforecast"],columns=['day','rainChance','mmRainMax','weatherdescription'])
 print(df2)
-
-
-             # "C°
-
-        
